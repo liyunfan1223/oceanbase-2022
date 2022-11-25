8e362e2d9d7a696156e5997dddd4c4817a5b933c
Yunfan Li <lyf_liyunfan@sina.com>
2022-11-25 14:58:17 +0800

    chore: modify buffer size

--- src/sql/engine/cmd/ob_load_data_direct_demo.h
+++ src/sql/engine/cmd/ob_load_data_direct_demo.h
@@ -191,8 +191,8 @@ private:
 
 class ObLoadDataDirectDemo : public ObLoadDataBase
 {
-  static const int64_t MEM_BUFFER_SIZE = (1LL << 32); // 1G -> 4G
-  static const int64_t FILE_BUFFER_SIZE = (2LL << 25); // 2M -> 32M
+  static const int64_t MEM_BUFFER_SIZE = (4LL << 30); // 1G -> 4G
+  static const int64_t FILE_BUFFER_SIZE = (32LL << 20); // 2M -> 32M
 public:
   ObLoadDataDirectDemo();
   virtual ~ObLoadDataDirectDemo();
