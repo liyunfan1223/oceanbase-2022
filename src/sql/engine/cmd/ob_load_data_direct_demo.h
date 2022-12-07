bb3cf27f6268e08adbb985d7efa325f97014c211
Yunfan Li <lyf_liyunfan@sina.com>
2022-12-07 23:43:37 +0800

    chore: params(256M, 32)

--- src/sql/engine/cmd/ob_load_data_direct_demo.h
+++ src/sql/engine/cmd/ob_load_data_direct_demo.h
@@ -163,7 +163,7 @@ private:
 
 class ObLoadSSTableWriter
 {
-  static const int64_t THREAD_POOL_SIZE = 16;
+  static const int64_t THREAD_POOL_SIZE = 32;
 public:
   ObLoadSSTableWriter();
   ~ObLoadSSTableWriter();
@@ -211,10 +211,10 @@ class ObLoadDataDirectDemo : public ObLoadDataBase
     public:
     ObLoadDataDirectDemo * ob_load_data_direct_demo;
   };
-  static const int64_t MEM_BUFFER_SIZE = (640 << 20);
+  static const int64_t MEM_BUFFER_SIZE = (256LL << 20);
   static const int64_t FILE_BUFFER_SIZE = (2LL << 20);
   static const int64_t ALLOCATOR_SIZE = FILE_BUFFER_SIZE << 2;
-  static const int64_t THREAD_POOL_SIZE = 16;
+  static const int64_t THREAD_POOL_SIZE = 32;
   static const int64_t SAMPLE_POOL_SIZE = 2000;
 public:
   ObLoadDataDirectDemo();
