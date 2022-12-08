dd5d82c0d05ca17150aa9ef746e05b726b6b0a94
Yunfan Li <lyf_liyunfan@sina.com>
2022-12-08 17:08:33 +0800

    chore: 512M, 2M, 8, 16, 2000

--- src/sql/engine/cmd/ob_load_data_direct_demo.cpp
+++ src/sql/engine/cmd/ob_load_data_direct_demo.cpp
@@ -1139,21 +1139,26 @@ void ObLoadDataDirectDemo::MyThreadPool2::run1()
       if (OB_FAIL(row_caster.get_casted_row(*ob_row_vec[i], datum_row))) {
         LOG_WARN("fail to cast row", KR(ret));
       } else {
-        ob_load_data_direct_demo->mutex_.lock();
-        if (!ob_load_data_direct_demo->sample_inited_) {
+        int sample_inited = ob_load_data_direct_demo->sample_inited_;
+        if (!sample_inited) {
+          ob_load_data_direct_demo->mutex_.lock();
+          sample_inited = ob_load_data_direct_demo->sample_inited_;
+          ob_load_data_direct_demo->mutex_.unlock();
+        }
+        if (!sample_inited) {
           const int64_t item_size = sizeof(ObLoadDatumRow) + datum_row->get_deep_copy_size();
           int64_t buf_pos = sizeof(ObLoadDatumRow);
           buf = static_cast<char *>(ob_load_data_direct_demo->allocators_[thread_id].alloc(item_size));
           new_item = new (buf) ObLoadDatumRow();
           new_item->deep_copy(*datum_row, buf, item_size, buf_pos);
+          ob_load_data_direct_demo->mutex_.lock();
           ob_load_data_direct_demo->datumrow_list_.push_back(new_item);
           sample_count++;
           if (sample_count == SAMPLE_POOL_SIZE) {
-              ob_load_data_direct_demo->generate_sample_datumrows();
-            }
+            ob_load_data_direct_demo->generate_sample_datumrows();
+          }
           ob_load_data_direct_demo->mutex_.unlock();
-        } else {
-          ob_load_data_direct_demo->mutex_.unlock();
+        }else {
           int bucket_index = 0;
           ob_load_data_direct_demo->get_bucket_index(datum_row, bucket_index, thread_id);
           ob_load_data_direct_demo->mutex_for_bucket_[bucket_index].lock();
@@ -1207,8 +1212,12 @@ int ObLoadDataDirectDemo::do_load()
 
 int ObLoadDataDirectDemo::generate_sample_datumrows()
 {
-  sample_inited_ = true;
   int ret = OB_SUCCESS;
+  if (sample_inited_) {
+    LOG_WARN("duplicate inited.");
+    return ret;
+  }
+  sample_inited_ = true;
   std::sort(datumrow_list_.begin(), datumrow_list_.end(), compare_);
   for (int i = 1; i < TOTAL_BUCKET_NUM; i++) {
     sample_datumrows_.push_back(datumrow_list_[(datumrow_list_.size() - 1) / TOTAL_BUCKET_NUM * i]);
